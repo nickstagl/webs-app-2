e35e7773b77ac8c167b3ca2cf9041ba1834a145f
nickstagl <nick.stagl@kellogg.northwestern.edu>
2026-02-19 03:30:20 +0000

    end of class, posts lab

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -20,7 +20,13 @@ class CompaniesController < ApplicationController
   def create
      # start with a new Company
      # assign user-entered form data to Company's columns
+      company = Company.new
+      company["name"] = params["name"]
+      company["city"] = params["city"]
+      company["state"] = params["state"]
+     
      # save Company row
+      company.save
      # redirect user
      redirect_to "/companies"
   end
